f8d0f3d6ec98544c18578ac4cf4eac84180b6db2
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2015-05-29 21:34:52 -0400

    Started search

--- main.js
+++ main.js
@@ -3,13 +3,13 @@
 
 define(function (require, exports, module) {
     "use strict";
-    
+
     /**
     * Constants
     */
-    
+
     var EXTENSION_NAME = "Todoer";
-    
+
     /**
     * Variables
     */   
@@ -19,6 +19,7 @@ define(function (require, exports, module) {
         WorkspaceManager   = brackets.getModule('view/WorkspaceManager'),
         ExtensionUtils     = brackets.getModule('utils/ExtensionUtils'),
         AppInit            = brackets.getModule('utils/AppInit'),
+        FindInFiles        = brackets.getModule('search/FindInFiles'),
         todoerIcon         = $('<a title="' + EXTENSION_NAME + '" id="todoer-icon"></a>'),
         panelTemplate      = require('text!html/panel.html'),
         panel;
@@ -39,32 +40,32 @@ define(function (require, exports, module) {
 
         registerCommandHandler('bliitzkrieg.todoer.view', EXTENSION_NAME, togglePanel, 'Ctrl-Alt-Shift-T', viewMenu);
     }
-    
+
     /**
 	 * Creates the "Todoer's bottom panel.
 	 */
-	function createBottomPanel() {
-		panel = WorkspaceManager.createBottomPanel('bliitzkrieg.todoer.panel', $(panelTemplate), 100);
-	}
-    
+    function createBottomPanel() {
+        panel = WorkspaceManager.createBottomPanel('bliitzkrieg.todoer.panel', $(panelTemplate), 100);
+    }
+
     /**
      * Description: Adds event listeners.
      */
     function addHandlers() {
         todoerIcon.on('click', togglePanel).
-            appendTo('#main-toolbar .buttons');
+        appendTo('#main-toolbar .buttons');
     }
-    
+
     function togglePanel() {
     }
-    
+
     /**
      * Loads external stylesheets.
      */
     function addStyles() {
         ExtensionUtils.loadStyleSheet(module, 'css/todoer.css');
     }
-    
+
     /**
      * Description: Initialize the extension.
      */
@@ -73,8 +74,12 @@ define(function (require, exports, module) {
         addMenuCommands();
         addStyles();
         addHandlers();
-       // if (localStorage.getItem('georapbox.notes.visible') === 'true') {
+
+        var x = FindInFiles.doSearchInScope("// todo::", null, null, null, null);
+        
+        console.log(x);
+        // if (localStorage.getItem('georapbox.notes.visible') === 'true') {
         //    togglePanel();
-       // }
+        // }
     });
 });
